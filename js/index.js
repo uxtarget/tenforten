aa03e5cd2987b47f0d2dce99352a856d5b495238
Abraham Jagadeesh <abraham.jagadeesh@publicissapient.com>
2021-03-18 13:01:34 -0500

    Initial Commit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
 var nextBtns = document.querySelectorAll('.next-btn');
+var backBtns = document.querySelectorAll(".back-btn");
 var sections = document.querySelectorAll('.section');
 var sections_total = 3;
 
 for(var i = 0 ; i < nextBtns.length; i ++) {
     nextBtns[i].addEventListener('click', function(e) {clickHandler(e);});
 }
+for(var j = 0 ; j < backBtns.length; j ++) {
+    backBtns[j].addEventListener('click', function(e) {backClickHandler(e);});
+}
 
+function backClickHandler(e) {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+  var target = e.currentTarget;
+  var index = target.dataset["index"];
+  if (index < sections_total) {
+    sections[index - 1].classList.remove( "animate__fadeIn");
+    sections[index-2] && sections[index-2].classList.remove( "animate__fadeOut");
+  }
+}
 function clickHandler (e) {
     window.scrollTo({
       top: 0,
--- js/index2.js
+++ js/index2.js
@@ -1,4 +1,5 @@
 var nextBtns = document.querySelectorAll(".next-btn");
+var backBtns = document.querySelectorAll(".back-btn");
 var sections = document.querySelectorAll(".inner");
 var sections_total = 6;
 
@@ -7,11 +8,43 @@ for (var i = 0; i < nextBtns.length; i++) {
     clickHandler(e);
   });
 }
+for (var j = 0; j < backBtns.length; j++) {
+  backBtns[j].addEventListener("click", function (e) {
+    backClickHandler(e);
+  });
+}
+
+function backClickHandler(e) {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+  var target = e.currentTarget;
+  var index = target.dataset["index"];
+  if (index < sections_total) {
+    sections[index - 1].classList.remove(
+      "animate__animated",
+      "animate__slideInLeft",
+      "animate__slideOutLeft",
+      "animate__slideInRight"
+    );
+    // sections[index-1].classList.add("animate__slideOutLeft");
+    sections[index - 2].classList.remove(
+      // "animate__animated",
+      "animate__slower",
+      "animate__slideInLeft",
+      "animate__slideOutLeft"
+    );
+    // sections[index - 2].classList.add("animate__slideInRight");
+  }
+  return false;
+}
 
 function clickHandler(e) {
   var target = e.currentTarget;
   var index = target.dataset["index"];
   if (index < sections_total) {
+    sections[index - 1].classList.remove("animate__slideInRight");
     sections[index - 1].classList.add(
       "animate__animated",
       "animate__slower",
@@ -19,4 +52,5 @@ function clickHandler(e) {
     );
     sections[index].classList.add("animate__animated", "animate__slideInRight");
   }
+  return false;
 }
